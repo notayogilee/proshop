112553f636d2e3c9e1b09dfb01659a9f9902a0ab
Lee Castelani <notayogilee@gmail.com>
2020-11-11 17:57:02 -0500

    add JWT and password hashing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 const router = Router();
-import { authUser } from '../controllers/userController.js';
+import { authUser, registerUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
+router.route('/').post(registerUser);
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
