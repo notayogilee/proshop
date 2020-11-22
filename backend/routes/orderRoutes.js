734e1baf961276d4767528906cbf65a22632f582
Lee Castelani <notayogilee@gmail.com>
2020-11-22 14:42:11 -0500

    create order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+const router = Router();
+import { addOrderItems } from '../controllers/orderController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+router.route('/').post(protect, addOrderItems);
+
+export default router;
