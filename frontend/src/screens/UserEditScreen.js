ccf04f9800272d1663f06fce0aa4c85193ff4251
Lee Castelani <notayogilee@gmail.com>
2020-11-26 17:16:50 -0500

    admin edit user functional

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message.js';
 import Loader from '../components/Loader.js';
 import FormContainer from '../components/FormContainer.js';
-import { getUserDetails } from '../actions/userActions';
+import { getUserDetails, updateUser } from '../actions/userActions';
+import { USER_UPDATE_RESET } from '../constants/userConstants';
 
 const UserEditScreen = ({ match, history }) => {
   const userId = match.params.id;
@@ -19,7 +20,14 @@ const UserEditScreen = ({ match, history }) => {
   const userDetails = useSelector(state => state.userDetails)
   const { loading, error, user } = userDetails;
 
+  const userUpdate = useSelector(state => state.userUpdate)
+  const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate;
+
   useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET });
+      history.push("/admin/userList")
+    }
     if (!user.name || user._id !== userId) {
       dispatch(getUserDetails(userId))
     } else {
@@ -27,11 +35,11 @@ const UserEditScreen = ({ match, history }) => {
       setEmail(user.email);
       setIsAdmin(user.isAdmin);
     }
-  }, [user, dispatch, userId])
+  }, [user, dispatch, userId, successUpdate, history])
 
   const submitHandler = (e) => {
     e.preventDefault();
-
+    dispatch(updateUser({ _id: userId, name, email, isAdmin }))
   }
 
   return (
@@ -41,6 +49,8 @@ const UserEditScreen = ({ match, history }) => {
       </Link>
       <FormContainer>
         <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
         {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
           <Form onSubmit={submitHandler}>
             <Form.Group controlId="name">
