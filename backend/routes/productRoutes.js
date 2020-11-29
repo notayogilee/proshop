7fe8f98843825b9e8491d78b95a561269bb289ff
Lee Castelani <notayogilee@gmail.com>
2020-11-29 16:57:02 -0500

    add product review route for backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 const router = Router();
-import { getProductById, getProducts, deleteProduct, createProduct, updateProduct } from '../controllers/productController.js';
+import {
+  getProductById,
+  getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview
+} from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router
   .route('/')
   .get(getProducts)
   .post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 router
   .route('/:id')
   .get(getProductById)
