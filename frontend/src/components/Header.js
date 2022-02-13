f0d2d35158c22cfdd4b37fcd3b40d8a019f0f2fb
Lee Castelani <notayogilee@gmail.com>
2022-02-12 19:35:36 -0500

    add redirect to main page on logout. refresh page to clear state

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import SearchBox from './SearchBox';
 import { logout } from '../actions/userActions';
 
+
 const Header = () => {
   const dispatch = useDispatch();
+
+  const history = useHistory();
 
   const userLogin = useSelector(state => state.userLogin);
   const { userInfo } = userLogin;
 
   const logoutHandler = () => {
     dispatch(logout());
+    history.push('/');
+    window.location.reload(false);
   }
   return (
     <header>
@@ -37,8 +42,8 @@ const Header = () => {
                   <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                 </NavDropdown>
               ) : <LinkContainer to="/login">
-                  <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
-                </LinkContainer>}
+                <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+              </LinkContainer>}
               {userInfo && userInfo.isAdmin && (
                 <NavDropdown title="Admin" id="adminmenu">
                   <LinkContainer to="/admin/userlist">
